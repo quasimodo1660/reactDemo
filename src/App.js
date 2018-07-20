2c278e4b174510c1bca049219eb9ae17f655a91e
Quasimodo <quasimodo1660@gmail.com>
2018-07-20 10:39:29 -0700

    add some pakage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Navbar,NavItem,Icon} from 'react-materialize'
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <Navbar style={{backgroundColor:'black'}} fixed={true} brand={<img src={logo} width='60' className='App-logo' alt='logo' />} left>
+      
+        <NavItem href='get-started.html'>Getting started</NavItem>
+        <NavItem href='components.html'>Components</NavItem>
+      </Navbar>
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" width='5%' alt="logo" />
           <h1 className="App-title">Welcome to asbReact</h1>
-        </header>
+        </header> */}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      
     );
   }
 }
