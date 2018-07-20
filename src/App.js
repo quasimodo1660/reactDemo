4091b493f1e3e95040d36179b555b04d81545ec8
Quasimodo <quasimodo1660@gmail.com>
2018-07-20 14:12:44 -0700

    display current time

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Navbar,NavItem,Icon} from 'react-materialize'
+
 
 
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={curTime:null}
+  }
+  componentDidMount(){
+    setInterval(()=>{
+      this.setState({
+        curTime:new Date().toLocaleTimeString()
+      })
+    },1000)
+  }
+
   render() {
     return (
       <div className="App">
-      <Navbar style={{backgroundColor:'black'}} fixed={true} brand={<img src={logo} width='60' className='App-logo' alt='logo' />} left>
-      
-        <NavItem href='get-started.html'>Getting started</NavItem>
-        <NavItem href='components.html'>Components</NavItem>
-      </Navbar>
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" width='5%' alt="logo" />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to asbReact</h1>
-        </header> */}
+        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>The time is: {this.state.curTime}</p>
       </div>
       
     );
