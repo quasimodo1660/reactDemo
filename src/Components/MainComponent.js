ff81e8511f168d9802e2be937c5900a6ef6c743b
Quasimodo <quasimodo1660@gmail.com>
2018-08-19 23:49:34 -0700

    intro redux

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,49 +1,50 @@
 import React,{Component} from 'react'
 import CNavbar from './HeaderComponent'
-import {DISHES} from '../Shared/dishes'
-import {COMMENTS} from '../Shared/comments'
-import {LEADERS} from '../Shared/leaders'
-import {PROMOTIONS} from '../Shared/promotions'
 import Menu from './MenuComponent'
 import Dishdetail from './DishdetailComponent'
 import Footer from './FooterComponent'
 import Home from './HomeComponent'
 import Contact from './ContactComponent'
 import About from './AboutComponent'
-import {Switch, Route, Redirect} from 'react-router-dom'
+import {Switch, Route, Redirect, withRouter} from 'react-router-dom'
+import {connect} from 'react-redux'
+
+const mapStateToProps = state =>{
+     return {
+            dishes:state.dishes,
+            comments:state.comments,
+            promotions:state.promotions,
+            leaders:state.leaders
+     }   
+}
 
 
-
-
-export default class Main extends Component{
+class Main extends Component{
 
     constructor(props){
         super(props);
-        this.state={
-            dishes:DISHES,
-            comments:COMMENTS,
-            promotions:PROMOTIONS,
-            leaders:LEADERS
-        }
+
     }
+
+  
 
     render(){
 
         const HomePage=()=>{
             return (
                 <Home 
-                    dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
-                    promotion={this.state.promotions.filter((promotion)=>promotion.featured)[0]}
-                    leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
+                    dish={this.props.dishes.filter((dish)=>dish.featured)[0]}
+                    promotion={this.props.promotions.filter((promotion)=>promotion.featured)[0]}
+                    leader={this.props.leaders.filter((leader)=>leader.featured)[0]}
                 />
             )
         }
 
         const DishWithID=({match})=>{
             return (
-                <Dishdetail dish={this.state.dishes.filter((dish)=>
+                <Dishdetail dish={this.props.dishes.filter((dish)=>
                     dish.id=== parseInt(match.params.dishId,10))[0]} 
-                    comments={this.state.comments.filter((comment)=>comment.dishId===parseInt(match.params.dishId,10)
+                    comments={this.props.comments.filter((comment)=>comment.dishId===parseInt(match.params.dishId,10)
                     )} 
                 />
             )
@@ -57,9 +58,9 @@ export default class Main extends Component{
             <Switch>
                 <Route path='/home' component={HomePage} />
                 <Route exact path='/contactus' component={Contact} />
-                <Route exact path='/menu' component={()=><Menu dishes={this.state.dishes}/>} />
+                <Route exact path='/menu' component={()=><Menu dishes={this.props.dishes}/>} />
                 <Route path='/menu/:dishId' component={DishWithID} />
-                <Route exact path='/aboutus' component={()=><About leaders={this.state.leaders}/>} />
+                <Route exact path='/aboutus' component={()=><About leaders={this.props.leaders}/>} />
                 <Redirect to='/home' />
             </Switch>
             <Footer />
@@ -67,3 +68,5 @@ export default class Main extends Component{
         )
     }
 }
+
+export default withRouter(connect(mapStateToProps)(Main))
--- src/coursera.js
+++ src/coursera.js
@@ -1,16 +1,25 @@
 import React,{Component} from 'react'
 import Main from './Components/MainComponent'
 import {BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { ConfigureStore} from './redux/configureStore'
+
+
+const store=ConfigureStore()
 
 export default class Coursera extends Component{
+
+   
    
     render(){
         return (
+        <Provider store={store}>
         <BrowserRouter>
         <div>
             <Main />
         </div>
         </BrowserRouter>
+        </Provider>
         )
     }
 }
